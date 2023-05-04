1aeea667176737cb83edada6c62fda15e0f79fe9
JustANipple <prandisamuel99@gmail.com>
2023-05-04 17:41:18 +0200

    modified:   scripts/script.js

--- scripts/script.js
+++ scripts/script.js
@@ -61,11 +61,11 @@ function createEvents(data) {
   const activities = document.querySelectorAll(".dashboard_activity");
   for(const activity of activities) {
     //hover and unhover for activity
-    activity.addEventListener("mouseover", (event) => {
+    activity.addEventListener("mouseover", () => {
       activity.setAttribute("id", "mouseOver");
       activity.parentNode.style.transform = "translateY(-.5rem)";
     });
-    activity.addEventListener("mouseout", (event) => {
+    activity.addEventListener("mouseout", () => {
       activity.setAttribute("id", "");
       activity.parentNode.style.transform = "translateY(0)";
     });
@@ -73,10 +73,10 @@ function createEvents(data) {
   //hover and unhover for dots
   const dots = document.querySelectorAll(".header_dots");
   for(const dot of dots) {
-    dot.addEventListener("mouseover", (event) => {
+    dot.addEventListener("mouseover", () => {
       dot.style.filter = "brightness(1.5)";
     });
-    dot.addEventListener("mouseout", (event) => {
+    dot.addEventListener("mouseout", () => {
       dot.style.filter = "brightness(1)";
     });
   }
@@ -84,25 +84,50 @@ function createEvents(data) {
   const times = document.querySelectorAll(".header_report_time button");
   let clicked = 1;
   for(let i = 0; i < times.length; i++) {
-    times[i].addEventListener("mouseover", (event) => {
+    times[i].addEventListener("mouseover", () => {
       if(i !== clicked) {
         times[i].style.color = "white";
       }
     });
-    times[i].addEventListener("mouseout", (event) => {
+    times[i].addEventListener("mouseout", () => {
       if(i !== clicked) {
         times[i].style.color = "inherit";
       }
     });
     //click for time
-    times[i].addEventListener("click", (event) => {
+    const timeClicked = times[i];
+    times[i].addEventListener("click", () => {
       if(i !== clicked) {
         times[i].style.color = "white";
         times[clicked].style.color = "inherit";
         clicked = i;
       }
       //function to change numbers based on data
+      changeNumbers(data, timeClicked);
     });
+  }
+}
+
+function changeNumbers(data, timeClicked) {
+  const currentTimes = document.querySelectorAll(".time_current");
+  const lastTimes = document.querySelectorAll(".time_last");
+
+  const range = timeClicked.textContent.toLowerCase();
+  if(range === "daily") {
+    for(let i = 0; i < data.length; i++) {
+      currentTimes[i].textContent = data[i].timeframes.daily.current + "hrs";
+      lastTimes[i].textContent = "Last Day - " + data[i].timeframes.daily.previous + "hrs";
+    }
+  } else if (range === "weekly") {
+    for(let i = 0; i < data.length; i++) {
+      currentTimes[i].textContent = data[i].timeframes.weekly.current + "hrs";
+      lastTimes[i].textContent = "Last Week - " + data[i].timeframes.weekly.previous + "hrs";
+    }
+  } else if (range === "monthly") {
+    for(let i = 0; i < data.length; i++) {
+      currentTimes[i].textContent = data[i].timeframes.monthly.current + "hrs";
+      lastTimes[i].textContent = "Last Month - " + data[i].timeframes.monthly.previous + "hrs";
+    }
   }
 }
 
